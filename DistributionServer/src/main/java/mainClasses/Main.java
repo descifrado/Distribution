49d5ef92fd776c2d66b336e04359408ccf4f8e09
suraj-goel <surajgoel.1225@gmail.com>
2019-08-31 16:01:37 +0530

    Added jdbc connection

--- DistributionServer/src/main/java/mainClasses/Main.java
+++ DistributionServer/src/main/java/mainClasses/Main.java
@@ -1,7 +1,14 @@
 package mainClasses;
 
+import java.sql.Connection;
+
 public class Main {
+    static String user="root";
+    static String password="root";
+    static String host="jdbc:mysql://localhost:3306/Distribution";
+    static Connection connection=MysqlConnection.connect();
     public static void main(String[] args) {
-        System.out.println("Shrey");
+
     }
+
 }
