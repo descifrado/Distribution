2be323aba1c9e32e19a7a15818a8b2c5518f5201
suraj-goel <surajgoel.1225@gmail.com>
2019-08-31 22:10:51 +0530

    Added HandleClientRequest Class

---
+++ DistributionServer/src/main/java/mainClasses/HandleClientRequest.java
@@ -0,0 +1,28 @@
+package mainClasses;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class HandleClientRequest implements Runnable{
+    private Socket socket ;
+    ObjectOutputStream oos;
+    ObjectInputStream ois ;
+    public HandleClientRequest(Socket socket){
+        socket = this.socket;
+        try{
+            oos = new ObjectOutputStream(socket.getOutputStream());
+            ois = new ObjectInputStream(socket.getInputStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void run() {
+
+    }
+}
--- DistributionServer/src/main/java/mainClasses/Main.java
+++ DistributionServer/src/main/java/mainClasses/Main.java
@@ -1,5 +1,8 @@
 package mainClasses;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.Connection;
 
 public class Main {
@@ -8,6 +11,25 @@ public class Main {
     static String host="jdbc:mysql://localhost:3306/Distribution";
     static Connection connection=MysqlConnection.connect();
     public static void main(String[] args) {
+        ServerSocket serverSocket;
+        Socket socket;
+        try{
+            serverSocket = new ServerSocket(6963);
+            System.out.println("Server Started..!!");
+        }catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        while(true){
+            try{
+                socket = serverSocket.accept();
+                Thread t = new Thread(new HandleClientRequest(socket));
+                t.start();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 
