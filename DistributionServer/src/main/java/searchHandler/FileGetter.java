9a2ca6728ecaa6856184fa8684f5bbda72fa6042
Shrey Pandey <shreypandey1509@gmail.com>
2019-09-02 00:13:39 +0530

    Search Feature Added

--- DistributionServer/src/main/java/searchHandler/FileGetter.java
+++ DistributionServer/src/main/java/searchHandler/FileGetter.java
@@ -6,19 +6,34 @@ import mainClasses.Main;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class FileGetter {
     public static File getFileByUID(String UID){
-        File file;
+        File file=null;
         String query = "Select * from File where fileUID=?";
+        String tagQuery = "Select tags from FileTags where fileUID = ?";
         try {
             PreparedStatement statement = Main.connection.prepareStatement(query);
             statement.setString(1,UID);
             ResultSet resultSet=statement.executeQuery();
-        //    file=new File(UID,resultSet.getString("fileName"),resultSet.getString("location"),resultSet.getType("type"));
+            while (resultSet.next()) {
+                file = new File(UID, resultSet.getString("fileName"), resultSet.getString("type"), null);
+            }
+            statement = Main.connection.prepareStatement(tagQuery);
+            statement.setString(1,UID);
+            resultSet=statement.executeQuery();
+            Set<String> tags=new HashSet<String>();
+            while (resultSet.next()){
+                tags.add(resultSet.getString(1));
+            }
+            file.setTags(tags);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return file;
     }
 }
