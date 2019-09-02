6f13f9d626d7d8ed62dbf03872f2b018bb2c4f36
Shrey Pandey <shreypandey1509@gmail.com>
2019-09-02 21:54:52 +0530

    Hash Algorithm changed

--- DistributionClient/src/main/java/tools/HashGenerator.java
+++ DistributionClient/src/main/java/tools/HashGenerator.java
@@ -1,25 +1,43 @@
 package tools;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 
 public class HashGenerator {
-    public static String hash(String s){
-        String ans=null;
-        try {
-            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-            byte[] str=s.getBytes();
-            byte[] hashstr=messageDigest.digest(str);
-//            ans = new String(hashstr);
-            StringBuffer sb = new StringBuffer();
-            for(int i=0;i<str.length;i++){
-                sb.append(Integer.toString((str[i] & 0xff)+0x100,16)).substring(1);
-            }
-            return sb.toString();
-        } catch (NoSuchAlgorithmException e) {
+
+    private static String hash(String str)
+    {
+        String sha1 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+            crypt.reset();
+            crypt.update(str.getBytes("UTF-8"));
+            sha1 = byteToHex(crypt.digest());
+        }
+        catch(NoSuchAlgorithmException e)
+        {
             e.printStackTrace();
         }
-        return ans;
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    private static String byteToHex(final byte[] hash)
+    {
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
     }
 }
