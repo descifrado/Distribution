3319b177ba1cf0339e84dcff6bb67ba6f821750a
suraj-goel <surajgoel.1225@gmail.com>
2019-09-02 21:21:42 +0530

    Added downloadFile Controller and fxml file

---
+++ DistributionClient/src/main/java/controllers/Controller_DownloadFile.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class Controller_DownloadFile {
+    public JFXTextField searchbyname;
+    public JFXTextField searchbytype;
+    public JFXTextField searchbytag;
+
+    public void ondownloadclicked(ActionEvent actionEvent) {
+    }
+
+    public void onsearchclicked(ActionEvent actionEvent) {
+    }
+}
